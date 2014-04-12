85b78413ab0d6764c52891cea940dd2e4afd045d
heather huynh <amphy@uga.edu>
2014-04-12 15:35:18 -0400

    front end side commit
    base for webapp front end

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/foundation.css">
+		<link rel="stylesheet" href="css/foundation.min.css">
+	</head>
+<body>
